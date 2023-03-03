8310d478bbec06819ec1ace422ba4ecec16d23fe
unknown <sobakoved@inbox.lv>
2023-03-03 15:44:27 +0000

    Updates file structure, adds JSON and image folders

---
+++ src/components/NavTabs.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function NavTabs() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="gallery"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Gallery
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="blog"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Blog
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact/learn"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Learn
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
