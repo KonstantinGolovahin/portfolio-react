6d7244a581a7ce0c2bc0e3eed52ccc5870d59cb1
unknown <sobakoved@inbox.lv>
2023-03-06 22:44:21 +0000

    Adds styles to project cards

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 function FriendCard(props) {
   return (
-    <div className="card">
+    <div className="card projectcard">
       <div className="img-container">
-        <img alt={props.title} src={process.env.PUBLIC_URL +props.image} />
+        <img alt={props.title} src={process.env.PUBLIC_URL + props.image} />
       </div>
       <div className="content">
         <ul>
@@ -13,14 +13,24 @@ function FriendCard(props) {
             <strong>Name:</strong> {props.title}
           </li>
           <li>
-            <strong>Repo</strong> {props.github}
+            <a
+              className="btn btn-link btn-floating btn-lg  m-1"
+              href={props.github}
+              role="button"
+              data-mdb-ripple-color="dark"
+            >GitHub Repo</a>
           </li>
           <li>
-            <strong>Live URL</strong> {props.deploy}
+            <a
+              className="btn btn-link btn-floating btn-lg  m-1"
+              href={props.deploy}
+              role="button"
+              data-mdb-ripple-color="dark"
+            >Visit Live URL</a>
           </li>
-        </ul>
+                 </ul>
       </div>
-     
+
     </div>
   );
 }
